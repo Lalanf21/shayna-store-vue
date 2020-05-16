781335852b09fb549ea562a303a29a50a5d8d4dc
Lalanf21 <Lalanfathurrahman@gmail.com>
2020-05-17 02:03:12 +0700

    menambahkan plugin axios dan integrasi dengan API product

--- src/components/WomanProduct.vue
+++ src/components/WomanProduct.vue
@@ -3,94 +3,37 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" :dots="false" :nav="false" :autoplay="true">
-                        <div class="product-item">
+                        <div class="product-item" v-for="itemProduct in products" :key="itemProduct.id">
                             <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
+                                <img :src="itemProduct.gallery[0].photo" alt="" />
                                 <ul>
                                     <li class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
                                     </li>
                                     <li class="quick-view">
-                                        <router-link to="/product"> + Quick View </router-link>
+                                        <router-link :to="'/product/'+itemProduct.id"> + Quick View </router-link>
                                     </li>
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <router-link to="/product">
-                                    <h5>Guangzhou sweater</h5>
+                                <div class="catagory-name">{{ itemProduct.type }}</div>
+                                <router-link :to="'/product/'+itemProduct.id">
+                                    <h5>{{ itemProduct.name }}</h5>
                                 </router-link>
                                 <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
+                                    Rp.{{ itemProduct.price }}
+                                    <!-- <span>200</span> -->
                                 </div>
                             </div>
                         </div>
                     </carousel>
+                </div>
+                <div class="col-lg-12" v-else>
+                    <p>
+                        Barang terbaru belum tersedia
+                    </p>
                 </div>
             </div>
         </div>
@@ -100,11 +43,22 @@
 
 <script>
 import carousel from 'vue-owl-carousel';
+import axios from 'axios';
 
 export default {
     name: 'WomanProduct',
     components:{
         carousel
+    },
+    data(){
+        return{
+            products: []
+        }
+    },
+    mounted(){
+        axios.get("http://localhost/shayna_backend/public/api/products")
+            .then( res => (this.products = res.data.data.data))
+            .catch( err => console.log(err) );
     }
 }
 </script>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -29,43 +29,30 @@
                             <div class="product-pic-zoom">
                                 <img class="product-big-img" :src="gambar_default" />
                             </div>
-                            <div class="product-thumbs">
-                                <carousel :nav="false" dots="false" class="product-thumbs-track ps-slider">
-                                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' : '' " >
-                                        <img src="img/mickey1.jpg" alt="" />
+                            <div class="product-thumbs" v-if="productDetail.gallery.length > 0">
+                                <carousel :nav="false" dot="false" class="product-thumbs-track ps-slider">
+
+                                    <div 
+                                    v-for="productPhoto in productDetail.gallery"
+                                    :key="productPhoto.id"
+                                    class="pt" @click="changeImage(productPhoto.photo)" :class="productPhoto.photo == gambar_default ? 'active' : '' " >
+                                        <img :src="productPhoto.photo" alt="" />
                                     </div>
 
-                                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey2.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey3.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey4.jpg" alt="" />
-                                    </div>
                                 </carousel>
                             </div>
                         </div>
                         <div class="col-lg-6 text-left">
                             <div class="product-details">
                                 <div class="pd-title">
-                                    <span>oranges</span>
-                                    <h3>Pure Pineapple</h3>
+                                    <span>{{ productDetail.type }}</span>
+                                    <h3>{{ productDetail.name }}</h3>
                                 </div>
                                 <div class="pd-desc">
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                        {{ productDetail.description }}
                                     </p>
-                                    <p>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                                    </p>
-                                    <h4>$495.00</h4>
+                                    <h4>${{ productDetail.price }}</h4>
                                 </div>
                                 <div class="quantity">
                                     <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
@@ -91,31 +78,40 @@ import Header from "@/components/Header.vue";
 import RelatedProduct from "@/components/RelatedProduct.vue";
 import Footer from "@/components/Footer.vue";
 import carousel from 'vue-owl-carousel';
+import axios from 'axios';
 
 export default {
-  name: 'Product',
-  components: {
-    Header,
-    Footer,
-    RelatedProduct,
-    carousel
-  },
-  data(){
-      return {
-          gambar_default: "img/mickey1.jpg",
-          thumbs: [
-              "img/mickey1.jpg",
-              "img/mickey2.jpg",
-              "img/mickey3.jpg",
-              "img/mickey4.jpg"
-          ]
-      }
-  },
-  methods:{
-      changeImage(urlImg){
-          this.gambar_default = urlImg;
-      }
-  }
+    name: 'Product',
+    components: {
+        Header,
+        Footer,
+        RelatedProduct,
+        carousel
+    },
+    data(){
+        return {
+            gambar_default:"",
+            productDetail: []
+        }
+    },
+    methods:{
+        changeImage(urlImg){
+            this.gambar_default = urlImg;
+        },
+        setDataPhoto(data){
+            this.productDetail = data;
+            this.gambar_default = data.gallery[0].photo;
+        }
+    },
+     mounted(){
+        axios.get("http://localhost/shayna_backend/public/api/products",{
+            params: {
+                id: this.$route.params.id
+            }
+        })
+            .then( res => ( this.setDataPhoto(res.data.data) ) )
+            .catch( err => console.log(err) );
+    }
 }
 </script>
 
